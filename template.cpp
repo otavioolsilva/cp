3a626523e9f680f84e597e60e89530d7d82291f5
Otávio Silva <otavioolsilva@outlook.com>
2023-02-25 14:35:38 -0300

    template time agora vai

---
+++ template.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dbg_out() { cerr << endl; }
+template <typename H, typename... T> 
+void dbg_out(H h, T... t) { cout << ' ' << h; dbg_out(t...); }
+#define dbg(...) cout << #__VA_ARGS__ << ':'; dbg_out(__VA_ARGS__);
+#define ln cerr << "line:" << __LINE__ << " fine, thanks" << endl
+
+#define pb push_back
+#define mp make_pair
+#define fs first
+#define sc second
+
+#define vi vector<int>
+#define pii pair<int,int>
+#define all(x) x.begin(),x.end()
+
+#define int ll
+using ll = long long;
+#define endl '\n'
+
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3f
+#define oo 0x3f3f3f3f3f3f3f3f
+
+const int MOD = 1e9+7;
+const int MAXN = 2e5+10;
+
+signed main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+
+  return 0;
+}
+
