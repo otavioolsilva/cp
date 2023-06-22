ee1a943cd1930ecc56c6553e94526f65869fab42
Otávio Silva <otavioolsilva@gmail.com>
2023-06-22 00:00:04 -0300

    template time 'travazapp' e configurações do vim

--- template.cpp
+++ template.cpp
@@ -5,27 +5,11 @@ void dbg_out() { cerr << endl; }
 template <typename H, typename... T> 
 void dbg_out(H h, T... t) { cerr << ' ' << h; dbg_out(t...); }
 #define dbg(...) cerr << #__VA_ARGS__ << ':'; dbg_out(__VA_ARGS__);
-#define ln cerr << "line:" << __LINE__ << " fine, thanks" << endl
+#define ln cerr << "line:" << __LINE__ << ", ate aqui bao" << endl
 
-#define pb push_back
-#define mp make_pair
-#define fs first
-#define sc second
-
-#define vi vector<int>
-#define pii pair<int,int>
-#define all(x) x.begin(),x.end()
-
-#define int ll
-using ll = long long;
-#define endl '\n'
-
-#define INF 0x3f3f3f3f
-#define INFLL 0x3f3f3f3f3f3f3f3f
-#define oo 0x3f3f3f3f3f3f3f3f
-
-const int MOD = 1e9+7;
-const int MAXN = 2e5+10;
+typedef long long ll;
+const int INF = 0x3f3f3f3f, MAXN = 1e5+10;
+const ll MOD = 1e9+7, INFLL = 0x3f3f3f3f3f3f3f3f, oo = INFLL;
 
 signed main(){
   ios_base::sync_with_stdio(false);
